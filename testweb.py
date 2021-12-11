4b582ac96c6a59bdc5ad9f097b6ac6ba6614935f
Shankar S S <93978702+shankar-saradha@users.noreply.github.com>
2021-12-10 19:53:17 -0800

    Add files via upload

---
+++ testweb.py
@@ -0,0 +1,112 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+content = """
+<!DOCTYPE html>
+<html>
+<head>
+<title>Saveetha Engineering College</title>
+<link rel = "icon" href = 
+"https://media.discordapp.net/attachments/533340656987275284/906080541344956436/kissclipart-saveetha-university-chennai-logo-clipart-saveetha-3a90c06681798db2.png" 
+        type = "image/x-icon">
+</head>
+<body>
+
+<br><center><img src="https://media.discordapp.net/attachments/533340656987275284/906080541344956436/kissclipart-saveetha-university-chennai-logo-clipart-saveetha-3a90c06681798db2.png" alt="Saveetha Engineering College" style="width:100px;height:100px;"><h1>SAVEETHA ENGINEERING COLLEGE</center></h1></br>
+<br><h2>Student Detail</h2></br>
+<h3>Name: Shankar</h1>
+<h3>Reference Number: 21002057</h3>
+<h3>Department: B.Tech AI&ML</h6>
+<h3>Contact(Gmail): assassinshankar@gmail.com</h3>
+<center><br><br><div class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="copyright-text">
+                <p>
+                  © 2021
+                  <a href=""
+                    ><u>Python Web Hosting</u></a
+                  >. All rights reserved. Made With ❤ by
+                  <a href=""><u>SEC Students</u></a> and 
+                  <a
+                    href=""
+                    ><u>Shankar</u></a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></br></br></center>
+
+</body>
+  <style>
+    @font-face {
+      font-family: "Euclid Circular B Medium";
+      src: url("//db.onlinewebfonts.com/t/721c73bcd2e49f3a621991089838b503.eot");
+      src: url("//db.onlinewebfonts.com/t/721c73bcd2e49f3a621991089838b503.eot?#iefix")
+          format("embedded-opentype"),
+        url("//db.onlinewebfonts.com/t/721c73bcd2e49f3a621991089838b503.woff2")
+          format("woff2"),
+        url("//db.onlinewebfonts.com/t/721c73bcd2e49f3a621991089838b503.woff")
+          format("woff"),
+        url("//db.onlinewebfonts.com/t/721c73bcd2e49f3a621991089838b503.ttf")
+          format("truetype"),
+        url("//db.onlinewebfonts.com/t/721c73bcd2e49f3a621991089838b503.svg#Euclid Circular B Medium")
+          format("svg");
+    }
+
+    body {
+      background: linear-gradient(to right, #8a2387, #e94057, #f27121);
+      color: #fff;
+      font-family: "Euclid Circular B Medium", Poppins;
+    }
+     <style>
+  <script>
+    .footer {
+      padding: 32px 0;
+      position: absolute;
+      width: 100%;
+      background-color: red;
+      color: white;
+      text-align: center;
+      background: rgba(255, 255, 255, 0.5);
+      box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+      backdrop-filter: blur(9px);
+      -webkit-backdrop-filter: blur(9px);
+      border: 1px solid rgba(255, 255, 255, 0.18);
+    }
+
+    .footer p {
+      margin: 0;
+      line-height: 26px;
+      font-size: 15px;
+      color: #fff;
+    }
+
+    .footer p a {
+      background: linear-gradient(to right, #f27121, #e94057, #8a2387);
+      color: transparent;
+      -webkit-background-clip: text;
+      background-clip: text;
+      text-decoration: none;
+    }
+
+    .footer p a:hover {
+      color: white;
+    }
+  </style>
+  <script>
+
+</html>
+"""
+class myhandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("request received")
+        self.send_response(200)
+        self.send_header('content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
+server_address = ('',2004)
+httpd = HTTPServer(server_address,myhandler)
+print("Website is running...")
+httpd.serve_forever()
